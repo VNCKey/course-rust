4ba64d6ae8caa6b0948b825933d6f78ac0de310a
VNCKey <villanewcode@gmail.com>
2025-12-11 19:51:34 -0500

    install.adock agregado
    Signed-off-by: VNCKey <villanewcode@gmail.com>

--- src/variables.rs
+++ src/variables.rs
@@ -6,14 +6,13 @@ pub fn variables() {
 
 pub fn variables_mutability() {
     let mut x = 5;
-    
+
     println!("x = {}", x);
-    
+
     x += 1;
 
     println!("x = {}", x);
 }
-
 
 pub fn variables_shadowing() {
     let x = 5;
@@ -26,18 +25,18 @@ pub fn variables_shadowing() {
     println!("y = {}", y);
 }
 
-const VALOR_CONSTANTE = "Texto constante";
+const VALOR_CONSTANTE: &str = "Texto constante";
 
 pub static APP_NAME: &str = "MiApp";
 
-pub fn constant_and_static(){
+pub fn constant_and_static() {
     //se inlinea: cada uso es una copia del valor literal.
     println!("Constante: {}", VALOR_CONSTANTE);
     //tiene dirección única; todos los usos comparten la misma ubicación de memoria.
     println!("Static: {}", APP_NAME);
 }
 
-pub fn scope(){
+pub fn scope() {
     let x = 10;
     {
         let y = 20;
@@ -50,8 +49,8 @@ pub fn scalar_types() {
     println!("------Tipos escalares------");
     println!("Integers: i8, i16, i32, i64, i128, isize");
     let x: i32 = 1_000;
-    let x2 = -2_000;    // inferido i32
-    let x3 = 42i64;     // sufijo explícito
+    let x2 = -2_000; // inferido i32
+    let x3 = 42i64; // sufijo explícito
     println!("x = {}", x);
     println!("Unsigned integers: u8, u16, u32, u64, u128, usize");
     // sin signo
@@ -59,7 +58,7 @@ pub fn scalar_types() {
     println!("y = {}", y);
     println!("Floats: f32, f64");
     let z: f32 = 3.14;
-    let z1 = 3.14;      // inferido f64
+    let z1 = 3.14; // inferido f64
     println!("z = {}", z);
     println!("Boolean: bool");
     let b: bool = true;
