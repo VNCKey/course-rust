68af1bdde1b36e933109bc87863230cddb73b02f
VNCKey <villanewcode@gmail.com>
2025-12-23 22:59:17 -0500

    .

---
+++ src/js-rust/vai.js
@@ -0,0 +1,21 @@
+// declarar variables en js
+
+//var vnc = 5;
+
+let vnc = "Alexander";
+
+vnc = 5;
+
+console.log(vnc);
+
+vnc = "Alexander";
+
+console.log(vnc);
+
+const PI = 3.14159;
+
+//PI = 15;
+
+//const PI = 3.888;
+
+console.log(PI);
